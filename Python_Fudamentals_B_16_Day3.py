6b0816457663568dc55c7f980d659bfc856efa7f
abhisingh445 <63054256+abhisingh445@users.noreply.github.com>
2020-04-02 21:18:47 +0530

    Add files via upload

---
+++ Python_Fudamentals_B_16_Day3.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Introduction to f strings
+
+first_name='abhijeet'
+last_name='singh'
+full_name=f"{first_name} {last_name}"
+print(full_name)
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+# enchancement of the code
+print(full_name.title())
+
+
+# In[30]:
+
+
+message=f"Keep the good work, {first_name.title()} !!!"
+print(message)
+
+
+# In[31]:
+
+
+#adding white spaces to strings
+
+print('Python','Java','C++')
+
+
+# In[32]:
+
+
+print('\nPython','\nJava','\nC++')
+
+
+# In[40]:
+
+
+print('Important Programming Languages :','\n\t\t\t\t\tPython','\n\t\t\t\t\tJava','\n\t\t\t\t\tC++')
+
+
+# In[ ]:
+
+
+
+
